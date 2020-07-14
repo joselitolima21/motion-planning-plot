e03c592cf0a1e395d03d39715f4ef5810ad736c8
Joselito Lima <joselitolima90@hotmail.com>
2020-07-13 21:21:36 -0300

    #1 - Primeiro commit

---
+++ ws/build/moveit_tutorials/doc/state_display/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/joselito/Artigo-MoveIt/ws/src/moveit_tutorials/doc/state_display
+# Build directory: /home/joselito/Artigo-MoveIt/ws/build/moveit_tutorials/doc/state_display
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ws/src/panda_moveit_config/scripts/move.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+def all_close(goal, actual, tolerance):
+  
+  all_equal = True
+  if type(goal) is list:
+    for index in range(len(goal)):
+      if abs(actual[index] - goal[index]) > tolerance:
+        return False
+
+  elif type(goal) is geometry_msgs.msg.PoseStamped:
+    return all_close(goal.pose, actual.pose, tolerance)
+
+  elif type(goal) is geometry_msgs.msg.Pose:
+    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+  return True
+
+class MoveGroupPythonIntefaceTutorial(object):
+  def __init__(self):
+    super(MoveGroupPythonIntefaceTutorial, self).__init__()
+
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('move_group_python_interface_tutorial',
+                    anonymous=True)
+
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSceneInterface()
+
+    group_name = "panda_arm"
+    group = moveit_commander.MoveGroupCommander(group_name)
+
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+
+    planning_frame = group.get_planning_frame()
+    print "============ Reference frame: %s" % planning_frame
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = group.get_end_effector_link()
+    print "============ End effector: %s" % eef_link
+
+    # We can get a list of all the groups in the robot:
+    group_names = robot.get_group_names()
+    print "============ Robot Groups:", robot.get_group_names()
+
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+    print "============ Printing robot state"
+    print robot.get_current_state()
+    print ""
+    ## END_SUB_TUTORIAL
+
+    # Misc variables
+    self.box_name = ''
+    self.robot = robot
+    self.scene = scene
+    self.group = group
+    self.display_trajectory_publisher = display_trajectory_publisher
+    self.planning_frame = planning_frame
+    self.eef_link = eef_link
+    self.group_names = group_names
+
+  def go_to_joint_state(self):
+    group = self.group
+
+    joint_goal = group.get_current_joint_values()
+    joint_goal[0] = 0
+    joint_goal[1] = 0.6393
+    joint_goal[2] = -0.2547
+    joint_goal[3] = -2.412
+    joint_goal[4] = -0.9552
+    joint_goal[5] = 3.1932
+    joint_goal[6] = 1.8148
+
+    group.go(joint_goal, wait=True)
+    
+    joint_home = group.get_current_joint_values()
+    joint_home[0] = 0
+    joint_home[1] = -0.0194
+    joint_home[2] = -0.2547
+    joint_home[3] = -0.9605
+    joint_home[4] = -1.9103
+    joint_home[5] = 3.5246
+    joint_home[6] = -0.0318
+
+    group.go(joint_home, wait=True)
+    
+    group.stop()
+
+    current_joints = self.group.get_current_joint_values()
+    
+    return all_close(joint_goal, current_joints, 0.01)
+
+  def go_to_pose_goal(self):
+    group = self.group
+  
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.w = 1.0
+    pose_goal.position.x = 0.4
+    pose_goal.position.y = 0.1
+    pose_goal.position.z = 0.4
+    group.set_pose_target(pose_goal)
+
+    plan = group.go(wait=True)
+
+    group.stop()
+    group.clear_pose_targets()
+
+    current_pose = self.group.get_current_pose().pose
+    return all_close(pose_goal, current_pose, 0.01)
+
+
+def main():
+  try:
+    print "============ Iniciando o programa ============"
+    tutorial = MoveGroupPythonIntefaceTutorial()
+    tutorial.go_to_joint_state()
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return
+
+if __name__ == '__main__':
+  main()
+
+## BEGIN_TUTORIAL
+## .. _moveit_commander:
+##    http://docs.ros.org/kinetic/api/moveit_commander/html/namespacemoveit__commander.html
+##
+## .. _MoveGroupCommander:
+##    http://docs.ros.org/kinetic/api/moveit_commander/html/classmoveit__commander_1_1move__group_1_1MoveGroupCommander.html
+##
+## .. _RobotCommander:
+##    http://docs.ros.org/kinetic/api/moveit_commander/html/classmoveit__commander_1_1robot_1_1RobotCommander.html
+##
+## .. _PlanningSceneInterface:
+##    http://docs.ros.org/kinetic/api/moveit_commander/html/classmoveit__commander_1_1planning__scene__interface_1_1PlanningSceneInterface.html
+##
+## .. _DisplayTrajectory:
+##    http://docs.ros.org/kinetic/api/moveit_msgs/html/msg/DisplayTrajectory.html
+##
+## .. _RobotTrajectory:
+##    http://docs.ros.org/kinetic/api/moveit_msgs/html/msg/RobotTrajectory.html
+##
+## .. _rospy:
+##    http://docs.ros.org/kinetic/api/rospy/html/
+## CALL_SUB_TUTORIAL imports
+## CALL_SUB_TUTORIAL setup
+## CALL_SUB_TUTORIAL basic_info
+## CALL_SUB_TUTORIAL plan_to_joint_state
+## CALL_SUB_TUTORIAL plan_to_pose
+## CALL_SUB_TUTORIAL plan_cartesian_path
+## CALL_SUB_TUTORIAL display_trajectory
+## CALL_SUB_TUTORIAL execute_plan
+## CALL_SUB_TUTORIAL add_box
+## CALL_SUB_TUTORIAL wait_for_scene_update
+## CALL_SUB_TUTORIAL attach_object
+## CALL_SUB_TUTORIAL detach_object
+## CALL_SUB_TUTORIAL remove_object
+## END_TUTORIAL
