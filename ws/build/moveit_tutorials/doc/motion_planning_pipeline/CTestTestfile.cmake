5d3a6d9a94324f7ba7ce3c12b32de7529189cef9
Joselito Lima <joselitolima90@hotmail.com>
2020-07-13 21:23:28 -0300

    #1 - Primeiro commit

--- ws/build/moveit_tutorials/doc/motion_planning_pipeline/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joselito/Artigo-MoveIt/ws/src/moveit_tutorials/doc/motion_planning_pipeline
-# Build directory: /home/joselito/Artigo-MoveIt/ws/build/moveit_tutorials/doc/motion_planning_pipeline
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
