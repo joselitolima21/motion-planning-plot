2d7c85d493390dc9fdfefdba8b3cad49e49a00e3
Joselito Lima <joselitolima90@hotmail.com>
2020-07-17 14:07:36 -0300

    script para ouvir os dados

---
+++ ws/src/panda_moveit_config/scripts/echoData.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import JointState
+
+def callback(data):
+    rospy.loginfo(data.position)
+    rospy.loginfo(data.velocity)
+    
+def listener():
+    rospy.init_node('echo_joint_data', anonymous=True)
+    rospy.Subscriber("/joint_states", JointState, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
